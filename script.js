240e5f5b86421a81b05c6d4bdfd6e2a702872547
eleazar.fuentes <eleazar.fuentes@gmail.com>
2023-06-28 21:08:43 -0600

    added comments: generate button, click event, password lenght

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Assignment Code
+// this is the generate button
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -11,4 +12,20 @@ function writePassword() {
 }
 
 // Add event listener to generate button
+// this is the click event which goes to button
 generateBtn.addEventListener("click", writePassword);
+
+//This code generate a password
+function generatePassword() {
+  //choose password length
+  lenghtp = Number(window.prompt("Password Lenght? (min 8, max 128)", ""));
+  var length = lenghtp;
+  var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  var password = "";
+
+  for (var i = 0, n = charset.length; i < length; ++i) {
+    password += charset.charAt(Math.floor(Math.random() * n));
+  }
+  return password;
+}
+
