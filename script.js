ad1278e0472dc549d55c9eaa42aebb42401ee6de
eleazar.fuentes <eleazar.fuentes@gmail.com>
2023-06-29 13:42:56 -0600

    added comments: added coments to code

--- script.js
+++ script.js
@@ -22,20 +22,23 @@ function generatePassword() {
   var length = Number(window.prompt("Choose your password lenght? (min 8, max 128)", ""));
 
   if (length < 8 || length > 128) {
-    alert("Invalid password length. Password lenght must be between 8 to 128 characters. Please try again.");
+    alert("Invalid password length. Password lenght must be between 8 to 128 characters. Please try again."); //validate password lenght
     return;
   }
 
+  //variables to include lower, upper, numeric and/or special characters
   var lower = confirm("Would you like to include lowercase characters?");
   var upper = confirm("Would you like to include uppercase characters?");
   var numeric = confirm("Would you like to include numeric characters?");
   var special = confirm("Would you like to include special characters?");
 
+  //conditional to validate at least one type of character
   if (!lower && !upper && !numeric && !special) {
     alert("You must select at least one character type. Please try again.");
     return;
   }
 
+  //datasets for password generation
   var characterSet = '';
 
   if (lower) {
@@ -56,7 +59,9 @@ function generatePassword() {
 
   var password = '';
 
+  // passwor generation, 
   for (var i = 0; i < length; i++) {
+    //generates a random floating-point number, characterSet string determines the number of characters available for selection then multiplies the random number by the length of characterSet and rounds down a floating-point number to the nearest integer.
     var randomIndex = Math.floor(Math.random() * characterSet.length);
     password += characterSet.charAt(randomIndex);
   }
